204551e13b15172f819e928280265ce5bc674520
SiddharthAher2002 <ahersiddharth2002@gmail.com>
2024-10-25 12:40:48 +0530

    Created home page layout

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,4 +1,15 @@
 @extends('frontend.layouts.app')
-@section('content')
+@section('style')
+<style>
 
+</style>
+@endsection
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <img class="mw-100" src="{{asset('images/home.png')}}"/>
+        </div>
+    </div>
+</div>
 @endsection
---
+++ resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,48 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4">
+                <h5 class="text-uppercase">About Us</h5>
+                <p>
+                    Our Inventory Management System provides businesses with the tools to efficiently track and manage
+                    stock levels, streamline order processing, and generate insightful reports. With real-time inventory
+                    tracking and multi-location support.
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <h5 class="text-uppercase">Links</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#" class="text-dark">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-dark">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-dark">Services</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-dark">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <h5 class="text-uppercase">Follow Us</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#" class="text-dark">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-dark">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-dark">Instagram</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.1);">
+        &copy; {{ date('Y') }} Your Company. All rights reserved.
+    </div>
+</footer>
