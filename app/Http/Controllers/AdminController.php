aa1ff73614291dd4dc0af1d124b58ed615c4fc3f
SiddharthAher2002 <ahersiddharth2002@gmail.com>
2024-11-11 23:12:25 +0530

    Auth Validation

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use App\Services\UserService;
 class AdminController extends Controller
@@ -10,24 +11,43 @@ class AdminController extends Controller
 
     public function __construct(
         UserService $userService
-    ){
+    ) {
         $this->_userService = $userService;
     }
-    public function getAdminDashboard(){
+    public function getAdminDashboard()
+    {
         return view('admin.dashboard');
     }
 
-    public function showUsers(){
+    public function showUsers()
+    {
         return view('admin.user-module.users');
     }
 
-    public function getUsers(Request $request){
+    public function getUsers(Request $request)
+    {
         $data = $this->_userService->getAllUsers($request);
         return response()->json($data);
     }
 
-    public function showLoginForm(){
+    public function showLoginForm()
+    {
         return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $inputCredentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(Auth::attempt($inputCredentials)){
+            var_dump("yes");
+        }
+        else
+            var_dump("no");
+        die;
     }
 
 }
--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 Route::prefix('admin')->group(function(){
     Route::get('/',[AdminController::class,'getAdminDashboard'])->name('admin.dashboard');
-    Route::get('/login',[AdminController::class,'showLoginForm'])->name('admin.login');
+    Route::get('/login',[AdminController::class,'showLoginForm'])->name('admin.show.login');
+    Route::post('/login',[AdminController::class,'login'])->name('admin.validate.login');
     Route::get('/user',[AdminController::class,'showUsers'])->name('admin.list.users');
     Route::post('/user',[AdminController::class,'getUsers'])->name('admin.get.users');
 });
